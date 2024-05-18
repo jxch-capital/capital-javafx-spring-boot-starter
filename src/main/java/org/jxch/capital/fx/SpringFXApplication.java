039081797f3a689fb09318eb2ce80c0cb42a2972
jxch <qianbuhan@proton.me>
2024-05-19 02:30:50 +0800

    支持Stage的后置处理

--- src/main/java/org/jxch/capital/fx/SpringFXApplication.java
+++ src/main/java/org/jxch/capital/fx/SpringFXApplication.java
@@ -1,9 +1,11 @@
 package org.jxch.capital.fx;
 
+import cn.hutool.extra.spring.SpringUtil;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.jxch.capital.fx.processor.StagePostProcessor;
 import org.jxch.capital.fx.util.SpringFXUtil;
 import org.springframework.beans.BeansException;
 import org.springframework.boot.CommandLineRunner;
@@ -12,6 +14,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.core.annotation.AnnotationAwareOrderComparator;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
@@ -20,12 +23,18 @@ import java.util.Set;
 @Component
 public class SpringFXApplication extends Application implements CommandLineRunner, ApplicationContextAware, ApplicationListener<ContextClosedEvent> {
     private static Class<?> controllerClazz;
-    private ApplicationContext context;
+    private static ApplicationContext context;
 
     @Override
     public void start(Stage stage) {
         Scene scene = SpringFXUtil.getControllerScene(controllerClazz);
         stage.setScene(scene);
+        stage.setTitle(SpringUtil.getApplicationName());
+
+        context.getBeansOfType(StagePostProcessor.class).values().stream()
+                .sorted(new AnnotationAwareOrderComparator())
+                .forEach(processor -> processor.postProcess(stage));
+
         stage.show();
     }
 
@@ -48,7 +57,7 @@ public class SpringFXApplication extends Application implements CommandLineRunne
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.context = applicationContext;
+        context = applicationContext;
     }
 
     public static ApplicationContext run(Class<?> springBootConfigClazz, Class<?> controllerClazz, String... args) {
---
+++ src/main/java/org/jxch/capital/fx/processor/StagePostProcessor.java
@@ -0,0 +1,9 @@
+package org.jxch.capital.fx.processor;
+
+import javafx.stage.Stage;
+
+public interface StagePostProcessor {
+
+    void postProcess(Stage stage);
+
+}
